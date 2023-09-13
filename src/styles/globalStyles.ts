b440cf9250a526454b31ebe3e11a234415c99352
Enricky Hipólito <enricky.hipolito@gmail.com>
2023-09-13 13:27:34 -0300

    install and config jest

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,7 +1,3 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyles = createGlobalStyle`
-  body {
-    background-color: red;
-  }
-`;
+export const GlobalStyles = createGlobalStyle``;
